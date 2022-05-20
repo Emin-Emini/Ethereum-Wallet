9b5063d3ed3d613dddc564d6912d29256933260d
emin.emini <emin.emini@asynclabs.co>
2022-05-20 17:38:57 +0200

    Initial Commit

---
+++ Ethereum Wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ethereum Wallet
+//
+//  Created by Emin Emini on 20.05.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
